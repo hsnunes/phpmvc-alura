93abd3e791535d90c841ffe34d55b4df0735e3e7
Hilder Nunes <hildersn@gmail.com>
2022-05-08 16:14:20 -0300

    Separando html do codigo - criando views

---
+++ view/cursos/formulario.php
@@ -0,0 +1,12 @@
+
+<?php require __DIR__ . '/../inicio-html.php'; ?>
+
+    <form action="">
+        <div class="form-group">
+            <label for="descricao">Descrição:</label>
+            <input name="descricao" id="descricao" type="text" type="text" class="form-control">
+        </div>
+        <button class="btn btn-primary">Salvar</button>
+    </form>
+
+<?php require __DIR__ . '/../fim-html.php'; ?>
